5dc14034f0bae0127381bdefd4cf1f404610bbc0
James Wang <james@yangwang.us>
2015-02-27 16:16:22 -0800

    changed package name

--- package.js
+++ package.js
@@ -1,15 +1,15 @@
 Package.describe({
-  name: 'pauli:accounts-linkedin',
+  name: 'jamesfirst:accounts-linkedin',
   summary: "Accounts service for LinkedIn accounts",
-  version: "1.1.1",
-  git: "https://github.com/PauliBuccini/meteor-accounts-linkedin.git"
+  version: "1.1.2",
+  git: "https://github.com/StarLinkCo/meteor-accounts-linkedin"
 });
 
 Package.onUse(function(api) {
 
   api.use('accounts-base@1.1.3', ['client', 'server']);
   api.use('accounts-oauth@1.0.0', ['client', 'server']);
-  api.use('pauli:linkedin@1.1.1', ['client', 'server']);
+  api.use('jamesfirst:linkedin@1.1.5', ['client', 'server']);
 
   api.addFiles(['linkedin_login_button.css'], 'client');
   api.addFiles('linkedin_common.js', ['client', 'server']);
