1b543e99f8f6c0f14891d5601a7ccdc9e4a3570c
Pauli B <pauli.buccini@gmail.com>
2015-10-19 19:46:55 +0700

    version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'pauli:accounts-linkedin',
   summary: "Accounts service for LinkedIn accounts",
-  version: "1.1.2",
+  version: "1.2.0",
   git: "https://github.com/PauliBuccini/meteor-accounts-linkedin.git"
 });
 
@@ -12,7 +12,7 @@ Package.onUse(function(api) {
   api.imply('accounts-base', ['client', 'server']);
 
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('pauli:linkedin@1.1.2', ['client', 'server']);
+  api.use('pauli:linkedin@1.2.0', ['client', 'server']);
 
   api.addFiles(['linkedin_login_button.css'], 'client');
   api.addFiles('linkedin_common.js', ['client', 'server']);
@@ -28,7 +28,7 @@ Package.onTest(function(api) {
   api.use('http', ['server']);
   api.use('ryepdx:linkedin-fake@0.0.4');
   api.use('pauli:accounts-linkedin');
-  
+
   api.addFiles('linkedin_client_tests.js', 'client');
   api.addFiles('linkedin_server_tests.js', 'server');
 });
