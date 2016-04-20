b84d683b26feedfd3ca509f3d0d9dd1d97491adb
Po Buchi <PoBuchi@users.noreply.github.com>
2016-04-20 17:11:38 +0700

    git link change

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
   name: 'pauli:accounts-linkedin',
   summary: "Accounts service for LinkedIn accounts",
-  version: "1.3.0",
-  git: "https://github.com/PauliBuccini/meteor-accounts-linkedin.git"
+  version: "1.3.1",
+  git: "https://github.com/PoBuchi/meteor-accounts-linkedin.git"
 });
 
 Package.onUse(function(api) {
@@ -12,7 +12,7 @@ Package.onUse(function(api) {
   api.imply('accounts-base', ['client', 'server']);
 
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('pauli:linkedin@1.3.0', ['client', 'server']);
+  api.use('pauli:linkedin@1.3.1', ['client', 'server']);
 
   api.addFiles(['linkedin_login_button.css'], 'client');
   api.addFiles('linkedin_common.js', ['client', 'server']);
