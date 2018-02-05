091dd2e45ee22eaa0d571820519274b2bdd0f545
Po Buchi <po@ox2.io>
2018-02-05 14:06:05 +0700

    #27 fix loginWithLinkedin

--- linkedin.js
+++ linkedin.js
@@ -1,7 +1,7 @@
 Accounts.oauth.registerService('linkedin');
 
 if (Meteor.isClient) {
-  Meteor.loginWithLinkedIn = function(options, callback) {
+  Meteor.loginWithLinkedin = function(options, callback) {
     // support a callback without options
     if (! callback && typeof options === "function") {
       callback = options;
