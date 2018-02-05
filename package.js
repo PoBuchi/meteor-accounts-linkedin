55ec9fcc6347e03d7b5a2066ae343beec844b5ce
Po Buchi <po@ox2.io>
2018-02-05 14:06:13 +0700

    build

--- package.js
+++ package.js
@@ -1,24 +1,24 @@
 Package.describe({
   name: 'pauli:accounts-linkedin',
-  summary: "Login service for LinkedIn accounts",
-  version: "2.1.5",
+  summary: "Login service for LinkedIn accounts, use with Meteor 1.6.1 & up",
+  version: "3.0.0",
   git: "https://github.com/PoBuchi/meteor-accounts-linkedin.git"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.4.3.1')
+  api.versionsFrom('1.6.1')
 
   api.use('accounts-base', ['client', 'server']);
   api.imply('accounts-base', ['client', 'server']);
 
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('pauli:linkedin-oauth@1.1.0', ['client', 'server']);
+  api.use('pauli:linkedin-oauth@3.0.0', ['client', 'server']);
 
   api.use('pauli:linkedin-oauth');
   api.imply('pauli:linkedin-oauth');
 
   // If users use accounts-ui but not facebook-config-ui, give them a tip.
-  api.use(['accounts-ui', 'pauli:linkedin-config-ui@1.1.1'], ['client', 'server'], { weak: true });
+  api.use(['accounts-ui', 'pauli:linkedin-config-ui@3.0.0'], ['client', 'server']);
   api.addFiles("notice.js");
 
   api.addFiles("linkedin.js");
